688e575077b52235d14b61a5d8ec400f9b2f45c8
Rajasekhar-TechnicalAssociate <ryarraguntla@evoketechnologies.com>
2023-09-15 15:57:37 +0530

    added api gateway

--- currency-conversion/src/main/java/com/raja/CurrencyExchangeProxy.java
+++ currency-conversion/src/main/java/com/raja/CurrencyExchangeProxy.java
@@ -1,13 +1,13 @@
 package com.raja;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+    @PostMapping("/currency-exchange")
+    public CurrencyConversion retrieveExchangeValue(@RequestBody CurrencyConversion conversion);
 
 }
--- currency-exchange/src/main/java/com/raja/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/raja/CurrencyExchangeController.java
@@ -5,9 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -19,15 +17,15 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+    @PostMapping("/currency-exchange")
+    public CurrencyExchange retrieveExchangeValue(@RequestBody CurrencyExchange exchange) {
 
-        log.info("retrieveExchangeValue called with {} to {}", from, to);
+        log.info("retrieveExchangeValue called with {} to {}", exchange.getFrom(), exchange.getTo());
 
-        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(exchange.getFrom(), exchange.getTo());
 
         if (currencyExchange == null) {
-            throw new RuntimeException("Unable to Find data for " + from + " to " + to);
+            throw new RuntimeException("Unable to Find data for " + exchange.getFrom() + " to " + exchange.getTo());
         }
 
         String port = environment.getProperty("local.server.port");

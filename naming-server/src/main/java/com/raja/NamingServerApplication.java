4aa458fe18dac840244c8428edb9349a13ba534c
Rajasekhar-TechnicalAssociate <ryarraguntla@evoketechnologies.com>
2023-09-15 15:15:49 +0530

    changed to maven project

--- naming-server/src/main/java/com/raja/NamingServerApplication.java
+++ naming-server/src/main/java/com/raja/NamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.raja.namingserver;
+package com.raja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- naming-server/src/test/java/com/raja/NamingServerApplicationTests.java
+++ naming-server/src/test/java/com/raja/NamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.raja.namingserver;
+package com.raja;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

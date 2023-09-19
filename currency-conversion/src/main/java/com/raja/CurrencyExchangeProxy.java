7dfa685e463eb49f47e09ead52c034d71f8b4ec4
Rajasekhar-TechnicalAssociate <ryarraguntla@evoketechnologies.com>
2023-09-19 18:24:25 +0530

    properties files updated

--- currency-conversion/src/main/java/com/raja/CurrencyExchangeProxy.java
+++ currency-conversion/src/main/java/com/raja/CurrencyExchangeProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "currency-exchange")
+@FeignClient(name = "CURRENCY-EXCHANGE")
 public interface CurrencyExchangeProxy {
 
     @PostMapping("/currency-exchange")

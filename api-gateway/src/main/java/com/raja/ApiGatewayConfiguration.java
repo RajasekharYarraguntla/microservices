88dd11a1db91a3ea595bad318d531b1e8551214c
Rajasekhar-TechnicalAssociate <ryarraguntla@evoketechnologies.com>
2023-09-19 10:33:16 +0530

    zipkin added for log tracing

---
+++ api-gateway/src/main/java/com/raja/ApiGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.raja;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes().route(p -> p.path("/get").uri("http://httpbin.org:80"))
+                .route( p -> p.path("/currency-conversion")
+                .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-exchange")
+                        .uri("lb://currency-exchange"))
+                .build();
+    }
+}
